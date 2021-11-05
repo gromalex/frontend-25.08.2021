46d5fcbb90437287d40c3fb7ee037c6a2895dbbc
Alexey Gulo <alexeygulo@gmail.com>
2021-11-05 21:34:26 +0300

    Lesson 9

---
+++ lesson-9 (with group)/app.js
@@ -0,0 +1,136 @@
+let data = []
+const formElement = document.querySelector('#form')
+const listElement = document.querySelector('#list')
+const listElements = {
+  freeGroup: document.querySelector('#freeGroup'),
+  worksGroup: document.querySelector('#worksGroup'),
+  personalGroup: document.querySelector('#personalGroup'),
+  homeGroup: document.querySelector('#homeGroup')
+}
+
+function handleSubmit (event) {
+  event.preventDefault()
+
+  const toDo = {
+    id: new Date().getTime(),
+    isChecked: false
+  }
+
+  const formData = new FormData(formElement)
+  for (let [name, value] of formData.entries()) {
+    toDo[name] = value
+  }
+
+  data.push(toDo)
+  formElement.reset()
+
+  console.log(data)
+
+  render()
+}
+
+function handleChange (event) {
+  const { target } = event
+  const { id, checked } = target
+
+  console.log(target.dataset)
+
+  data.forEach((item) => {
+    if (item.id == id) {
+      item.isChecked = checked
+    }
+  })
+
+  console.log(data)
+}
+
+function handleBeforeUnload () {
+  const json = JSON.stringify(data)
+  localStorage.setItem('data', json)
+}
+
+function handleDOMReady () {
+  const dataFromStorage = localStorage.getItem('data')
+
+  if (dataFromStorage) {
+    data = JSON.parse(dataFromStorage)
+
+    render()
+  }
+}
+
+function handleClickRemoveButton (event) {
+  // const { role, id } = event.target.dataset
+  const role = event.target.getAttribute('data-role')
+  const id = event.target.getAttribute('data-id')
+
+  if (role == 'remove') {
+    data = data.filter((item) => {
+      // if (item.id == id) {
+      //   return false
+      // } else {
+      //   return true
+      // }
+
+      return item.id != id
+    })
+
+    render()
+  }
+}
+
+// -------------------------------------------------------------------------------------------------
+
+function createToDoTemplate ({id, content, isChecked}) {
+  const checkedAttr = isChecked ? 'checked' : ''
+
+  const template = `
+    <div class="island__item d-flex">
+      <div class="form-check form-check-lg">
+        <input class="form-check-input" ${checkedAttr} type="checkbox" id="${id}">
+        <label class="form-check-label" for="${id}">${content}</label>
+      </div>
+
+      <button type="button" data-role="remove" data-id="${id}" class="btn btn-sm btn-danger ms-auto">
+        Удалить
+      </button>
+    </div>
+  `
+
+  return template
+}
+
+// function createToDoElements () {
+//   let result = ''
+
+//   data.forEach((toDo) => {
+//     result = result + createToDoTemplate(toDo)
+//   })
+
+//   return result
+// }
+
+function clearLists () {
+  for (let group in listElements) {
+    listElements[group].innerHTML = ''
+  }
+}
+
+function render () {
+  clearLists()
+
+  data.forEach((toDo) => {
+    const { group } = toDo
+    const listElement = listElements[group]
+
+    const template = createToDoTemplate(toDo)
+
+    listElement.innerHTML += template
+  })
+}
+
+formElement.addEventListener('submit', handleSubmit)
+listElement.addEventListener('change', handleChange)
+listElement.addEventListener('click', handleClickRemoveButton)
+window.addEventListener('beforeunload', handleBeforeUnload)
+window.addEventListener('DOMContentLoaded', handleDOMReady)
--- lesson-9/app.js
+++ lesson-9/app.js
@@ -1,5 +1,9 @@
 let data = []
-const formElement = document.querySelector('#form')
+let isEdit = false
+let currentEditedToDo = {}
+const formCreateElement = document.querySelector('#formCreate')
+const formEditElement = document.querySelector('#formEdit')
+const formEditContainerElement = document.querySelector('#formEditContainer')
 const listElement = document.querySelector('#list')
 
 function handleSubmit (event) {
@@ -10,20 +14,22 @@ function handleSubmit (event) {
     isChecked: false
   }
 
-  const formData = new FormData(formElement)
+  const formData = new FormData(formCreateElement)
   for (let [name, value] of formData.entries()) {
     toDo[name] = value
   }
 
   data.push(toDo)
-  formElement.reset()
+  formCreateElement.reset()
 
   render()
 }
 
 function handleChange (event) {
   const { target } = event
-  const { id, checked } = target
+  const { id, checked, type } = target
+
+  if (type !== 'checkbox') return
 
   console.log(target.dataset)
 
@@ -33,7 +39,7 @@ function handleChange (event) {
     }
   })
 
-  console.log(data)
+  render()
 }
 
 function handleBeforeUnload () {
@@ -71,21 +77,97 @@ function handleClickRemoveButton (event) {
   }
 }
 
+function handleClickEditButton (event) {
+  const { target } = event
+  const { role, id } = target.dataset
+
+  if (role == 'edit') {
+    if (isEdit == true) {
+      alert('Уже редактируется')
+      return
+    }
+
+    data.forEach((item) => {
+      if (item.id == id) {
+        currentEditedToDo = item
+        const { parentElement } = target
+        const formEditElement = formEditTemplate(item)
+
+        parentElement.outerHTML = formEditElement
+        isEdit = true
+      }
+    })
+  }
+}
+
+function handleClickCancelEditButton (event) {
+  const { role } = event.target.dataset
+
+  if (role == 'cancelEdit') {
+    render()
+    isEdit = false
+  }
+}
+
+function handleFormEditSubmit (event) {
+  event.preventDefault()
+  const { target } = event
+  const { role, id } = target.dataset
+
+  if (role == 'editForm') {
+    const content = target.querySelector('[name="content"]').value
+    console.log(content)
+    currentEditedToDo.content = content
+    console.log(currentEditedToDo)
+
+    render()
+    isEdit = false
+  }
+}
+
+
+
 // -------------------------------------------------------------------------------------------------
 
 function createToDoTemplate ({id, content, isChecked}) {
   const checkedAttr = isChecked ? 'checked' : ''
 
   const template = `
-    <div class="island__item d-flex">
+    <div class="island__item d-flex ${checkedAttr}">
       <div class="form-check form-check-lg">
         <input class="form-check-input" ${checkedAttr} type="checkbox" id="${id}">
         <label class="form-check-label" for="${id}">${content}</label>
       </div>
 
-      <button type="button" data-role="remove" data-id="${id}" class="btn btn-sm btn-danger ms-auto">
-        Удалить
+      <button type="button" data-role="edit" data-id="${id}" class="btn btn-sm btn-primary ms-auto">
+        <svg class="pe-none" width="16" height="16"><use href="#pencil"/></svg>
       </button>
+
+      <button type="button" data-role="remove" data-id="${id}" class="btn btn-sm btn-danger ms-3">
+        <svg class="pe-none" width="16" height="16"><use href="#trash"/></svg>
+      </button>
+    </div>
+  `
+
+  return template
+}
+
+function formEditTemplate ({ content }) {
+  const template = `
+    <div class="island__item" id="formEditContainer">
+      <form class="d-flex" data-role="editForm">
+        <div class="flex-grow-1">
+          <input type="text" class="form-control form-control-sm" name="content" placeholder="Введите задачу" required value="${content}">
+        </div>
+        <div class="ms-3">
+          <button type="submit" class="btn btn-sm btn-primary">
+            <svg class="pe-none" width="16" height="16"><use href="#save"/></svg>
+          </button>
+          <button type="button" data-role="cancelEdit" class="btn btn-sm btn-warning ms-3">
+            <svg class="pe-none" width="16" height="16"><use href="#cancel"/></svg>
+          </button>
+        </div>
+      </form>
     </div>
   `
 
@@ -107,8 +189,11 @@ function render () {
   listElement.innerHTML = toDoElements
 }
 
-formElement.addEventListener('submit', handleSubmit)
+formCreateElement.addEventListener('submit', handleSubmit)
 listElement.addEventListener('change', handleChange)
 listElement.addEventListener('click', handleClickRemoveButton)
+listElement.addEventListener('click', handleClickEditButton)
+listElement.addEventListener('click', handleClickCancelEditButton)
+listElement.addEventListener('submit', handleFormEditSubmit)
 window.addEventListener('beforeunload', handleBeforeUnload)
 window.addEventListener('DOMContentLoaded', handleDOMReady)
