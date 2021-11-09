4f285c4db99f8872c30578f5733607e12092a3a3
Alexey Gulo <alexeygulo@gmail.com>
2021-11-09 21:15:01 +0300

    Lesson 10

---
+++ lesson-10/script.js
@@ -0,0 +1,68 @@
+// Протокол запроса для Веб ---------------------------------------------------
+// http - протокол передачи гипертекста (HTTP) является базовым сетевым protocol, который позволяет передавать гипермедиа документы в Web
+// http://google.com - используя данную запись в url строке браузера, мы говорим: запросить документ используя протокол http.
+// https - защищённая версия протокола http
+// https://kunegin.com/ref3/wap/images/ris2.jpg
+
+// JSON (JavaScript Object Notation) ----------------------------------------------
+// Метод JSON.parse(response) - из json в объект javascript
+// Метод JSON.stringify(data) - из объекта javascript в json
+
+// Rest API
+//   - API - программный интерфейс для общения программ
+//   - REST - это интерфейс общения через HTTP протокол
+
+// CRUD: C - create, R - read, U - update, D - delete;
+
+// AJAX (Async JavaScript and XML) XMLHttpRequest — это встроенный в браузер объект, который даёт возможность делать HTTP-запросы к серверу без перезагрузки страницы -------------------------------------------------------------
+
+// Методы запроса
+// POST - create / update
+// GET - read
+// PUT/PATCH - update
+// DELETE - delete
+
+// Статусы запросов
+// 1хх - информационный
+// 2хх - успешное выполнение
+// 3хх - перенаправление
+// 4хх - ошибка на клиенте
+// 5хх - ошибка на сервере
+
+function fetchData (url, method, callback) {
+  const xhr = new XMLHttpRequest()
+
+  xhr.open(method, url)
+
+  xhr.onload = function () {
+    if (xhr.status == 200) {
+      callback(xhr.response)
+    }
+  }
+
+  xhr.send()
+}
+
+fetchData('https://jsonplaceholder.typicode.com/posts', 'GET', (response) => {
+  const data = JSON.parse(response)
+  const containerElement = document.querySelector('#container')
+
+  const cards = data.map((item) => {
+    return cardTemplate(item.title, item.body, item.id)
+  })
+
+  const result = cards.join('\n')
+
+  containerElement.innerHTML = result
+})
+
+function cardTemplate (title, text, id) {
+  return `
+    <div class="card" style="width: 25%" data-id="${id}">
+      <div class="card-body">
+        <h5 class="card-title">${title}</h5>
+        <p class="card-text">${text}</p>
+      </div>
+    </div>
+  `
+}
